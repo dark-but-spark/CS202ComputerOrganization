c57fb538fd51ca79b2b56482ad6fe0aec9429857
chen jiyan <3036789610@qq.com>
2025-03-03 14:54:41 +0800

    renamed:    lab/EGO1_User_Manual_v04_20161221.pdf -> EGO1_User_Manual_v04_20161221.pdf 	renamed:    "lab/Minisys\347\241\254\344\273\266\346\211\213\345\206\2141.1.pdf" -> "Minisys\347\241\254\344\273\266\346\211\213\345\206\2141.1.pdf" 	deleted:    lab/lab1/lab1_introduction_of_toolkits(2).pdf 	deleted:    lab/lab2/lab2_RISCV_data_details.pdf 	deleted:    lab/lab3/lab3_RISCV_calculation_branch_instruction.pdf 	renamed:    lab/lab1/a_tailed_risc-v_CPU_CSE.bit -> lab1/a_tailed_risc-v_CPU_CSE.bit 	renamed:    lab/lab1/lab1_p1_2.asm -> lab1/lab1_p1_2.asm 	renamed:    lab/lab1/lab1_practice1_2/lab1_p1_2.asm -> lab1/lab1_practice1_2/lab1_p1_2.asm 	renamed:    lab/lab1/lab1_practice1_2/out.txt -> lab1/lab1_practice1_2/out.txt 	renamed:    lab/lab1/out.txt -> lab1/out.txt 	renamed:    lab/lab2/DataInterpretation2.asm -> lab2/DataInterpretation2.asm 	renamed:    lab/lab2/DataInterpretation3.asm -> lab2/DataInterpretation3.asm 	renamed:    lab/lab2/DataInterpretation4.asm -> lab2/DataInterpretation4.asm 	renamed:    lab/lab2/Load2.asm -> lab2/Load2.asm 	renamed:    lab/lab2/QuickStart2.asm -> lab2/QuickStart2.asm 	renamed:    lab/lab2/Store2.asm -> lab2/Store2.asm 	renamed:    lab/lab2/SystemCall.asm -> lab2/SystemCall.asm 	renamed:    lab/lab2/macro_print_str.asm -> lab2/macro_print_str.asm 	renamed:    lab/lab2/practice1.asm -> lab2/practice1.asm 	renamed:    lab/lab2/practice2.asm -> lab2/practice2.asm 	renamed:    lab/lab2/practice3.asm -> lab2/practice3.asm 	renamed:    lab/lab3/Branch.asm -> lab3/Branch.asm 	renamed:    lab/lab3/LogicalOperation1.asm -> lab3/LogicalOperation1.asm 	renamed:    lab/lab3/LogicalOperation2.asm -> lab3/LogicalOperation2.asm 	renamed:    lab/lab3/Loop.asm -> lab3/Loop.asm 	renamed:    lab/lab3/macro_print_str.asm -> lab3/macro_print_str.asm 	deleted:    lecture/CompOrg_25S_Lec1_CourseInfo.pdf 	deleted:    lecture/CompOrg_25S_Lec1_Introduction.pdf 	deleted:    lecture/CompOrg_25S_Lec2_RISC-V_Intro.pdf 	deleted:    lecture/CompOrg_25S_Lec3_RISC-V_Procedure.pdf

--- lab3/Loop.asm
+++ lab3/Loop.asm
@@ -14,3 +14,19 @@ public class CaculateSum{
 .include "macro_print_str.asm"
 .data
     #.....
+.text
+    add t1,zero,zero
+    addi t0,zero,1
+    addi t3,zero,10
+    calcu:
+        addi t0,t0,1 #i++
+        add t1,t1,t0 #sum+=i
+        bne t0,t3,calcu #i<=10
+    print_string("Sum of 1 to 10 is ")
+    mv a0,t1
+    li a7,1
+    ecall
+
+    li a7,10    
+    ecall
+    
